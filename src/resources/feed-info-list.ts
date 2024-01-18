a4cc15957f7a668b4e8ea2ee36222c730a2dfea4
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2024-01-18 15:45:50 -0800

    chore: lint

--- src/resources/feed-info-list.ts
+++ src/resources/feed-info-list.ts
@@ -21,12 +21,12 @@ const createFeedInfoList = (feedInfoTuples: [label: string, url: string, flags?:
 export const FEED_INFO_LIST: FeedInfo[] = createFeedInfoList([
   // ['番組名', 'RSS/AtomフィードのURL'],
   ['10X.fm', 'https://anchor.fm/s/559fd878/podcast/rss'],
-  ['LayerX NOW!','https://anchor.fm/s/55a49944/podcast/rss'],
+  ['LayerX NOW!', 'https://anchor.fm/s/55a49944/podcast/rss'],
   ['Texta.Fm - Design And Engineering Team At PIXTA', 'https://anchor.fm/s/330a9488/podcast/rss'],
   ['UIT INSIDE', 'https://uit-inside.linecorp.com/feed.atom'],
   ['聴くエンジニアType', 'https://anchor.fm/s/70ab9954/podcast/rss'],
   ['グリテンラジオ', 'https://anchor.fm/s/4c3bd64c/podcast/rss'],
   ['サイボウズ フロントエンド通信', 'https://anchor.fm/s/ec10d0c8/podcast/rss'],
   ['白金鉱業.FM', 'https://shirokane-kougyou.github.io/feed.xml'],
-  ['絶望エンジニアチャンネル', 'https://anchor.fm/s/daf96c00/podcast/rss']
+  ['絶望エンジニアチャンネル', 'https://anchor.fm/s/daf96c00/podcast/rss'],
 ]);
